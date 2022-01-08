6eff73d3942877c64cb4f61708baa3a5376790cf
Michael Davidis <mdavidis@cluster.gr>
2022-01-08 19:08:24 +0200

    Created messenger service

---
+++ assignment/src/Service/MessengerService.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Service;
+
+class MessengerService {
+
+    /**
+     * Send message to queue.
+     *
+     * @since 1.0.0
+     * @param string $message Message to be sent.
+     * @return void
+     */
+    public function sendMessage( $message ){
+
+    }
+
+}
