2d9234f0813c28c85a2fba9c67fbf4b6f65f9b95
Michael Davidis <mdavidis@cluster.gr>
2022-01-08 18:09:56 +0200

    Created first connection to the API

--- assignment/src/Controller/ApiController.php
+++ assignment/src/Controller/ApiController.php
@@ -2,6 +2,31 @@
 
 namespace App\Controller;
 
-class ApiController{
-    
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiController extends AbstractController  {
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client) {
+        $this->client = $client;
+    }
+
+    /**
+     * @Route("/")   
+     * @Method({"GET"});
+     */
+    public function getFromApi(){
+        $response = $this->client->request(
+            'GET',
+            'https://a831bqiv1d.execute-api.eu-west-1.amazonaws.com/dev/results'
+        );
+
+        return new Response($response->getContent());
+    }
+
 }
